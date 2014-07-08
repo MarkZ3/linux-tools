6604b426a3837d7b6ab941304b82d04d706a8bbe
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2014-07-08 19:38:20 -0400

    lttng: Convert performance results from Derby to JSON
    The performance tests and conversion can be executed from the command line:
    mvn clean install -Pperformance
    
    To connect to the database, additional properties have to be set.
    See perf.database.loc and perf.config in
    org.eclipse.linuxtools.lttng.alltests/pom.xml
    
    A destination can also be specified with -DperfFilesDestination
    
    Change-Id: I70b83b151d739eee7964cceef3f2b3d8078561b9
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

---
+++ lttng/org.eclipse.linuxtools.lttng.alltests/src/org/eclipse/linuxtools/lttng/alltests/PerfResultsToJSon.java
@@ -0,0 +1,136 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Marc-Andre Laperle - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.linuxtools.lttng.alltests;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.eclipse.test.internal.performance.InternalDimensions;
+import org.eclipse.test.internal.performance.PerformanceTestPlugin;
+import org.eclipse.test.internal.performance.db.DB;
+import org.eclipse.test.internal.performance.db.Scenario;
+import org.eclipse.test.internal.performance.db.TimeSeries;
+import org.eclipse.test.internal.performance.db.Variations;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+/**
+ * Convert results from the database to JSON suitable for display
+ */
+public class PerfResultsToJSon {
+
+    private static final String BUILD_DATE_FORMAT = "yyyyMMdd-HHmm";
+
+    private static final String OVERVIEW_CHART_FILE_NAME = "chart_overview0";
+    private static final String CHART_FILE_NAME = "chart";
+    private static final String CHART_FILE_NAME_EXTENSION = ".json";
+
+    /**
+     * Convert results from the database to JSON suitable for display
+     *
+     * @throws JSONException
+     *             JSON error
+     * @throws IOException
+     *             io error
+     */
+    @Test
+    public void parseResults() throws JSONException, IOException {
+        Variations variations = PerformanceTestPlugin.getVariations();
+
+        String scenarioPattern = "%"; //$NON-NLS-1$
+
+        String seriesKey = PerformanceTestPlugin.BUILD;
+        List<String> builds = new ArrayList<>();
+        DB.queryDistinctValues(builds, seriesKey, variations, scenarioPattern);
+
+        Scenario[] scenarios = DB.queryScenarios(variations, scenarioPattern, seriesKey, null);
+        System.out.println("Converting results for " + scenarios.length + " Scenarios"); //$NON-NLS-1$
+
+        JSONArray root = new JSONArray();
+        for (Scenario s : scenarios) {
+            root.put(createSerie(s, variations));
+        }
+        try (FileWriter fw = new FileWriter(OVERVIEW_CHART_FILE_NAME + CHART_FILE_NAME_EXTENSION)) {
+            fw.write(root.toString());
+        }
+
+        for (int i = 0; i < scenarios.length; i++) {
+            JSONArray rootScenario = new JSONArray();
+            rootScenario.put(createSerie(scenarios[i], variations));
+            try (FileWriter fw = new FileWriter(CHART_FILE_NAME + i + CHART_FILE_NAME_EXTENSION)) {
+                fw.write(rootScenario.toString());
+            }
+        }
+    }
+
+    /**
+     * This main can be run from within Eclipse provided everything is on the
+     * class path.
+     *
+     * @param args
+     *            the arguments
+     * @throws JSONException
+     *             JSON error
+     * @throws IOException
+     *             io error
+     */
+    public static void main(String[] args) throws JSONException, IOException {
+        new PerfResultsToJSon().parseResults();
+    }
+
+    private static JSONObject createSerie(Scenario s, Variations variations) throws JSONException {
+        JSONObject o = new JSONObject();
+        o.putOpt("key", s.getScenarioName());
+        o.putOpt("values", createPoints(s, variations));
+        return o;
+    }
+
+    private static JSONArray createPoints(Scenario s, Variations variations) throws JSONException {
+        // Can be uncommented to see raw dump
+        // s.dump(System.out, PerformanceTestPlugin.BUILD);
+
+        String[] builds = DB.querySeriesValues(s.getScenarioName(), variations, PerformanceTestPlugin.BUILD);
+        Date[] dates = new Date[builds.length];
+        for (int i = 0; i < builds.length; i++) {
+            dates[i] = parseBuildDate(builds[i]);
+        }
+
+        TimeSeries timeSeries = s.getTimeSeries(InternalDimensions.CPU_TIME);
+        JSONArray dataPoints = new JSONArray();
+        for (int i = 0; i < dates.length; i++) {
+            JSONArray point = new JSONArray();
+            point.put(dates[i].getTime());
+            point.put(timeSeries.getValue(i));
+            dataPoints.put(point);
+        }
+        return dataPoints;
+    }
+
+    private static Date parseBuildDate(String build) {
+        SimpleDateFormat f = new SimpleDateFormat(BUILD_DATE_FORMAT);
+        Date date;
+        try {
+            date = f.parse(build);
+        } catch (ParseException e) {
+            return null;
+        }
+        return date;
+    }
+}
